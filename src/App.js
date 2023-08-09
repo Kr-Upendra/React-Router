f7dcd9b21f8b7844366e632355e7c9ce6d73ae26
Upendra Kumar <toxic74412@gmail.com>
2023-08-09 19:06:54 +0530

    added footer component

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="app">
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/blogs/:slug" element={<Blog />} />
-        </Routes>
+        <div className="appin">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blogs/:slug" element={<Blog />} />
+          </Routes>
+        </div>
+        <Footer />
       </Router>
     </div>
   );
